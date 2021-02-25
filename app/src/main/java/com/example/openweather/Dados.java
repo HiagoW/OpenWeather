6f0c2c0fd18b4c56528e8fd7be01c4120d7a6c9d
Hiago W Petris <hiago.petris@gmail.com>
2021-02-25 16:29:11 -0300

    Atualização

---
+++ app/src/main/java/com/example/openweather/Dados.java
@@ -0,0 +1,15 @@
+package com.example.openweather;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Dados {
+    private Main main;
+
+    public Main getMain() {
+        return main;
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+}
